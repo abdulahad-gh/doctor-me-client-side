7c2c34ac1d8d0f8fc66cca22a3077fced577b06b
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-04-17 01:32:26 +0600

    added banner section with responsive

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
-const Home = () => {
+import './Home.css'
+import { Link } from 'react-router-dom'; const Home = () => {
     return (
-        <div>
-            <h2>This is home</h2>
+        <div className='banner'>
+            <div class="banner-desc">
+                <h1>I'm Dr.Karim Ullah</h1>
+                <p>heyi!! this is Karim. MBBS, FCPS (Surgery), Fellowship (Hepatobiliary Pancreatic Surgery and Liver Transplant)</p>
+                <Link class="know-more-btn" to="/">Hit to Contact....</Link>
+            </div>
         </div>
     );
 };
--- src/pages/shared/Navbar/Navbar.js
+++ src/pages/shared/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
             <div class="container">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
