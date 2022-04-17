5e470353fda88f8e28a373d25bbc47105692ad11
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-04-17 16:38:43 +0600

    create json data,and completed services sectiuon

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
-import { Link } from 'react-router-dom'; const Home = () => {
+import { Link } from 'react-router-dom'; import Service from '../Service/Service';
+const Home = () => {
+    const [services, setServices] = useState([])
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+    console.log(services)
     return (
-        <div className='banner'>
-            <div class="banner-desc">
-                <h1>I'm Dr.Karim Ullah</h1>
-                <p>heyi!! this is Karim. MBBS, FCPS (Surgery), Fellowship (Hepatobiliary Pancreatic Surgery and Liver Transplant)</p>
-                <Link class="know-more-btn" to="/">Hit to Contact....</Link>
+        <>
+            <div className='banner'>
+                <div class="banner-desc">
+                    <h1>I'm Dr.Karim Ullah</h1>
+                    <p>heyi!! this is Karim. MBBS, FCPS (Surgery), Fellowship (Hepatobiliary Pancreatic Surgery and Liver Transplant)</p>
+                    <Link class="know-more-btn" to="/">Hit to Contact....</Link>
+                </div>
             </div>
-        </div>
+            <div className='pt-5 container'>
+                <h2 className='text-center py-5'>My Services</h2>
+                <div className='row justify-content-center gap-4'>
+                    {
+                        services.map(service => <Service service={service} key={service.id} />)
+                    }
+                </div>
+            </div>
+        </>
     );
 };
 
---
+++ src/pages/Service/Service.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Service = ({ service }) => {
+    const { name, desc, img, price } = service
+
+    return (
+        <div className="card position-relative p-2 shadow" style={{ width: '18rem' }}>
+            <img className="card-img-top" height={200} src={img} alt="service-img" />
+            <div className="card-body">
+                <h5 className="card-title">{name}</h5>
+                <p className="card-text">{desc}</p>
+                <p className="card-text mb-5">${price}</p>
+                <Link to="/" className="btn btn-primary position-absolute bottom-0 my-2">Check Out</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
